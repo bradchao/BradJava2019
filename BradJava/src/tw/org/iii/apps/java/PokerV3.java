58e5c9903ac9614b344648d43b73422b21c84a61
user <user@WTWANG-MBP>
2019-12-22 15:25:51 +0800

    12221535

--- BradJava/src/tw/org/iii/apps/java/PokerV3.java
+++ BradJava/src/tw/org/iii/apps/java/PokerV3.java
@@ -1,5 +1,7 @@
 package tw.org.iii.apps.java;
 
+import java.util.Arrays;
+
 public class PokerV3 {
 	public static void main(String[] args) {
 		int[] poker = new int[52];
@@ -12,20 +14,27 @@ public class PokerV3 {
 			poker[rand] = poker[i];
 			poker[i] = temp;
 		}
-		for (int v : poker) {
-			System.out.println(v);
-		}
-		System.out.println("----");
+		//for (int v : poker) {
+		//	System.out.println(v);
+		//}
+		//System.out.println("----");
 		
 		int[][] players = new int[4][13];
 		for (int i=0; i<poker.length; i++) {
 			players[i%4][i/4] = poker[i];
 		}
 		
-		for (int v : players[0]) {
-			System.out.println(v);
+		String[] suits = {"黑桃","紅心","方塊","梅花",};
+		String[] values = {"A ","2 ","3 ","4 ","5 ","6 ","7 ",
+				"8 ","9 ","10","J ","Q ","K ",};
+		for (int[] player : players) {
+			Arrays.sort(player);
+			for (int card : player) {
+				System.out.print(
+						suits[card/13] + values[card%13] + " ");
+			}
+			System.out.println();
 		}
-		
 		
 		
 		
