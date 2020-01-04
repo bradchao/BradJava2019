a91e6f21d03aeb91bbf7656a7e8aeddeca57fd92
user <user@WTWANG-MBP.r1804.tciii.org.tw>
2020-01-04 15:41:37 +0800

    01041541

--- BradJava/src/tw/brad/apps/Brad34.java
+++ BradJava/src/tw/brad/apps/Brad34.java
@@ -2,7 +2,11 @@ package tw.brad.apps;
 
 public class Brad34 {
 	public static void main(String[] args) {
-
+		Brad341 obj1 = new Brad344();
+		Brad341 obj2 = new Brad345();
+		Brad344 obj3 = new Brad344();
+		System.out.println(obj3 instanceof Brad344);
+		System.out.println(obj3 instanceof Brad341);
 	}
 }
 interface Brad341 {
@@ -15,4 +19,19 @@ interface Brad342 {
 }
 interface Brad343 extends Brad341, Brad342 {
 	void m4();
+}
+class Brad344 implements Brad341, Brad342 {
+	public void m1() {}
+	public void m2() {}
+	public void m3() {}
+}
+class Brad345 implements Brad343 {
+	public void m1() {}
+	public void m2() {}
+	public void m3() {}
+	public void m4() {}
+}
+
+abstract class Brad346 {
+	void m1() {}
 }
---
+++ BradJava/src/tw/brad/apps/Brad35.java
@@ -0,0 +1,53 @@
+package tw.brad.apps;
+
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Brad35 extends JFrame {
+	private JTextField mesg;
+	
+	public Brad35() {
+		super("Brad35");
+		
+		setLayout(new BorderLayout());
+		mesg = new JTextField();
+		add(mesg, BorderLayout.NORTH);
+		
+		JPanel content = new JPanel();
+		add(content, BorderLayout.CENTER);
+		
+		content.addMouseListener(new MyListener(this));
+		
+		setVisible(true);
+		setSize(640, 480);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void setMesg(String info) {
+		mesg.setText(info);
+	}
+	public static void main(String[] args) {
+		new Brad35();
+	}
+
+}
+
+class MyListener extends MouseAdapter {
+	private Brad35 win;
+	public MyListener(Brad35 win) {
+		this.win = win;
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		//System.out.println("Click:" + e.getX() + ", " + e.getY());
+		win.setMesg(e.getX() + ", " + e.getY());
+	}
+	
+}
+
+
