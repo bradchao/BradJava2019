7eec24f8d42a34959551fe8d58ebf00aaf4e79ee
user <user@WTWANG-MBP.r1804.tciii.org.tw>
2020-01-04 10:34:31 +0800

    01041034

--- BradJava/src/tw/brad/apps/Brad28.java
+++ BradJava/src/tw/brad/apps/Brad28.java
@@ -20,9 +20,20 @@ public class Brad28 extends JFrame implements ActionListener{
 		exit = new JButton("Exit");
 		
 		add(open); add(close); add(exit);
+
+		MyButtonListener2 myButtonListener2 = new MyButtonListener2();
+		open.addActionListener(myButtonListener2);
+
 		
 		open.addActionListener(this);
+		close.addActionListener(this);
 		
+		MyButtonListener myButtonListener = new MyButtonListener();
+		exit.addActionListener(myButtonListener);
+		
+		open.addActionListener(myButtonListener);
+
+
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(640, 480);
@@ -30,11 +41,34 @@ public class Brad28 extends JFrame implements ActionListener{
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("OK");
+		if (e.getSource() == open) {
+			System.out.println("OK0");	
+		}else if (e.getSource() == close) {
+			System.out.println("OK1");
+		}
+		
 	}
 	
 	public static void main(String[] args) {
 		new Brad28();
 	}
 
+}
+
+class MyButtonListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("OK2");
+	}
+	
+}
+
+class MyButtonListener2 implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("OK2222");
+	}
+	
 }
