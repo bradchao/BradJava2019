e9eb9a53fdac2cc6b04576b3fd9e4137a9bea5ec
user <user@10.0.103.93>
2020-01-12 11:56:25 +0800

    01121156

---
+++ BradJava/src/tw/brad/apps/MyEditor.java
@@ -0,0 +1,103 @@
+package tw.brad.apps;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class MyEditor extends JFrame {
+	private JButton open, save, saveas;
+	private JTextArea jta;
+	
+	public MyEditor() {
+		super("My Editor");
+	
+		setLayout(new BorderLayout());
+		JPanel topLine = new JPanel(new FlowLayout());
+		open = new JButton("Open"); topLine.add(open); 
+		save = new JButton("Save"); topLine.add(save); 
+		saveas = new JButton("Save as"); topLine.add(saveas);
+		add(topLine, BorderLayout.NORTH);
+		
+		jta = new JTextArea();
+		JScrollPane jsp = new JScrollPane(jta);
+		add(jsp, BorderLayout.CENTER);
+		
+		initEvent();
+		
+		setSize(640, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private void initEvent() {
+		open.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				openFile();
+			}
+		});
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveFile();
+			}
+		});
+		saveas.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveasFile();
+			}
+		});
+	}
+	
+	private File openFile;
+	
+	private void openFile() {
+		JFileChooser jfc = new JFileChooser(new File("."));
+		if (jfc.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+			openFile = jfc.getSelectedFile();
+			readFile();
+		}
+	}
+
+	private void readFile() {
+		if (openFile == null) return;
+		
+		try {
+			BufferedReader reader = new BufferedReader(
+					new FileReader(openFile));
+			jta.setText(""); String line;
+			while ( (line = reader.readLine()) != null) {
+				jta.append(line + "\n");
+			}
+			reader.close();
+		}catch(Exception e) {
+			System.out.println(e.toString());
+		}
+		
+		
+	}
+	
+	private void saveFile() {
+		
+	}
+	
+	private void saveasFile() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new MyEditor();
+	}
+}
