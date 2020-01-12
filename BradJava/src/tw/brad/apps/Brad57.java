cbf9ed682a4ee716be7ee8a99b99c019e1a8b077
user <user@10.0.103.93>
2020-01-12 11:01:05 +0800

    01121101

--- BradJava/src/tw/brad/apps/Brad57.java
+++ BradJava/src/tw/brad/apps/Brad57.java
@@ -1,6 +1,9 @@
 package tw.brad.apps;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 public class Brad57 {
 
@@ -17,6 +20,20 @@ public class Brad57 {
 		}catch(Exception e) {
 			System.out.println(e.toString());
 		}
+		
+		System.out.println("----------------");
+		
+		try {
+			ObjectInputStream oin = 
+				new ObjectInputStream(
+					new FileInputStream("dir2/brad.object"));
+			Object obj2 = oin.readObject();
+			oin.close();
+			System.out.println("read OK");
+		}catch(Exception e) {
+			System.out.println(e.toString());
+		}
+		
 	}
 
 }
@@ -26,6 +43,6 @@ class Brad571 {
 class Brad572 extends Brad571 {
 	Brad572(){System.out.println("Brad572()");}
 }
-class Brad573 extends Brad572 {
+class Brad573 extends Brad572 implements Serializable {
 	Brad573(){System.out.println("Brad573()");}
 }
