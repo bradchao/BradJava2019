56a0cc69c7f4885ca3172504924905a54dfcc5aa
user <user@10.0.103.93>
2020-01-12 13:40:02 +0800

    01121339

--- BradJava/src/tw/brad/apps/MyEditor.java
+++ BradJava/src/tw/brad/apps/MyEditor.java
@@ -1,7 +1,9 @@
 package tw.brad.apps;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -11,6 +13,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -19,7 +22,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class MyEditor extends JFrame {
-	private JButton open, save, saveas;
+	private JButton open, save, saveas, chcolor;
 	private JTextArea jta;
 	
 	public MyEditor() {
@@ -30,9 +33,12 @@ public class MyEditor extends JFrame {
 		open = new JButton("Open"); topLine.add(open); 
 		save = new JButton("Save"); topLine.add(save); 
 		saveas = new JButton("Save as"); topLine.add(saveas);
+		chcolor = new JButton("Change Coloe"); topLine.add(chcolor);
 		add(topLine, BorderLayout.NORTH);
 		
 		jta = new JTextArea();
+		jta.setForeground(nowColor);
+		jta.setFont(new Font("", Font.BOLD + Font.ITALIC, 24));
 		JScrollPane jsp = new JScrollPane(jta);
 		add(jsp, BorderLayout.CENTER);
 		
@@ -62,6 +68,19 @@ public class MyEditor extends JFrame {
 				saveasFile();
 			}
 		});
+		chcolor.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				changeColor();
+			}
+		});
+	}
+	
+	private Color nowColor = Color.BLUE;
+	private void changeColor() {
+		nowColor = JColorChooser.showDialog(this, "Change Color", nowColor);
+		jta.setForeground(nowColor);
+		
 	}
 	
 	private File openFile;
@@ -96,24 +115,29 @@ public class MyEditor extends JFrame {
 		if (openFile == null) {
 			saveasFile();
 		}else {
-			try {
-				BufferedWriter writter = new BufferedWriter(
-						new FileWriter(openFile));
-				writter.write(jta.getText());
-				writter.flush();
-				writter.close();
-				JOptionPane.showMessageDialog(this, "Save OK");
-			}catch(Exception e) {
-				System.out.println(e.toString());
-			}
+			writeFile();
 		}
-		
-		
-		
 	}
 	
 	private void saveasFile() {
-		
+		JFileChooser jfc = new JFileChooser(new File("."));
+		if (jfc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+			openFile = jfc.getSelectedFile();
+			writeFile();
+		}
+	}
+	
+	private void writeFile() {
+		try {
+			BufferedWriter writter = new BufferedWriter(
+					new FileWriter(openFile));
+			writter.write(jta.getText());
+			writter.flush();
+			writter.close();
+			JOptionPane.showMessageDialog(this, "Save OK");
+		}catch(Exception e) {
+			System.out.println(e.toString());
+		}
 	}
 	
 	public static void main(String[] args) {
